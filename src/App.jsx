b6b7e58dbe50123264ed574913bef45e699eea65
Feyz Ibrahim <faizashrafkk@gmail.com>
2024-01-18 16:20:03 +0530

    docker | Containerized

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
